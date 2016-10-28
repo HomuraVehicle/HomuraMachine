bf5525dd0d77764e2315fca296ce655e5246776d
hmito <hmito@outlook.com>
2016-10-28 09:25:12 +0100

    Add IO

---
+++ HomuraMachine/Mihara/IO.hpp
@@ -0,0 +1,15 @@
+#ifndef HMR_MACHINE_IO_INC
+#define HMR_MACHINE_IO_INC 100
+#
+namespace hmr{
+	namespace machine{
+		struct cIO{
+		public:
+			void operator()(void){
+
+			}
+		};
+	}
+}
+#
+#endif
