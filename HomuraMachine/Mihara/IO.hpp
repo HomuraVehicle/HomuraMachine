c10a8e4abbc4a85dc24e506f21f3bcaf255eba34
hmito <hmito@outlook.com>
2016-11-17 23:32:43 +0000

    Add cDualUart for control two uart.

--- HomuraMachine/Mihara/IO.hpp
+++ HomuraMachine/Mihara/IO.hpp
@@ -16,117 +16,30 @@ namespace hmr{
 	namespace machine{
 		namespace mihara{
 			//=== 基本定義 ===
-		#define RF_BaudRate 9600
-		#define RFDebug_BaudRate 38400
-		#define MP_BaudRate 38400
+			#define RF_BaudRate 9600
+			#define RFDebug_BaudRate 38400
+			#define MP_BaudRate 38400
 
-			template<typename io_device_>
-			struct cUartSwitcher: public io_device_{
-				typedef cUartSwitcher<io_device_> this_type;
-			private:
-				//ロックフラグ
-				bool Lock;
-			public:
+			namespace io{
 				//ModuleID
-				typedef enum{
-					null = 0x00,
-					mobile_phone = 0x01,
-					rf_module = 0x02
-				}rf_module_mode;
-			private:
-				//レジスタ系
-				xc32::interrupt_uart<RF0_uart_register_> Uart0;
-				xc32::interrupt_uart<RF1_uart_register_> Uart1;
-				RF0_power PinPowerUart0;
-				RF1_power PinPowerUart1;
-			public:
-				bool lock(){
-					if(is_lock())return false;
-					Lock = true;
-
-					Uart0.lock( 9600, xc32::uart::flowcontrol::rts_cts_control, SendInterrupt, RecvInterrupt);
-					Uart1.lock(38400, xc32::uart::flowcontrol::rts_cts_control, SendInterrupt, RecvInterrupt);
-
-					Uart0.recv_disable();
-					Uart0.send_disable();
-					Uart1.recv_disable();
-					Uart1.send_disable();
-
-					devmng::courier::uart::fput_set_interrupt();//streamVMC_set_fput_interrupt_flag();//set_interrupt_fputflag(Stream_VMC);//割り込み予約
-					devmng::interrupt_enable_streamVMC_fget_interrupt();// enable_interrupt_fget(Stream_VMC);
-					devmng::interrupt_enable_streamVMC_fput_interrupt();// enable_interrupt_fput(Stream_VMC);
-
-					return false;
-				}
-				bool is_lock()const{ return Lock; }
-				void unlock(){
-					Lock = false;
-
-					//割り込み禁止
-					devmng::courier::uart::fget_disable_interrupt();
-					devmng::courier::uart::fput_disable_interrupt();
-				}
-			};
+				enum module_mode{
+					module_null = 0x00,
+					module_mobile = 0x01,
+					module_rf = 0x02
+				};
+				struct module_selector_interface{
+					virtual bool setModuleMode(module_mode Mode_) = 0;
+					virtual module_mode getModuleMode()const = 0;
+				};
+			}
 			template<typename io_device_>
-			struct cIO :public message_host_interface{
+			struct cIO :public message_host_interface, public io::module_selector_interface, public io_device_{
 				typedef cIO<io_device_> this_type;
 			private:
-				//ロックフラグ
-				bool Lock;
-			private:
-				//=== モード制御機能 ===
-				typedef enum{ NormalMode, SleepMode, RoamingMode }mode;
-				//デバイスモードを変更する
-				void mode_set(mode Mode_);
-				//デバイスモードを取得する
-				volatile mode mode_get(void);
-			private:
-				//VMC1
-				VMC1* pVMC;
-				//受け取り用データ
-				idata IData;
-				bool IPacketMode = 0;
-				odata OData;
-				bool OPacketMode = 0;
-				//メッセージホルダー
-				cMessage Message;
-			public:
-				cIO(){}
-				bool lock(){
-					if(is_lock())return false;
-					Lock = true;
-
-					//通信関連機能の初期化
-					pVMC = com::createVMC1();
-					vmc1_initialize(pVMC, (const unsigned char*)("hmr"), (const unsigned char*)("ctr"));
-					com::initialize();
-
-					devmng::courier::uart::fput_set_interrupt();//streamVMC_set_fput_interrupt_flag();//set_interrupt_fputflag(Stream_VMC);//割り込み予約
-					devmng::interrupt_enable_streamVMC_fget_interrupt();// enable_interrupt_fget(Stream_VMC);
-					devmng::interrupt_enable_streamVMC_fput_interrupt();// enable_interrupt_fput(Stream_VMC);
-
-
-					return false;
-				}
-				bool is_lock()const{ return Lock; }
-				void unlock(){
-					Lock = false;
-
-					//割り込み禁止
-					devmng::courier::uart::fget_disable_interrupt();
-					devmng::courier::uart::fput_disable_interrupt();
-
-					//通信関連の終端化処理
-					vmc1_finalize(pVMC);
-					com::finalize();
-					com::releaseVMC1(pVMC);
-				}
-			public:
-				virtual void regist(message::element& rElement_){
-					Message.regist(rElement_);
-				}
-			private:
 				struct tx_interrupt{
+				private:
+					this_type& Ref;
+				public:
 					void operator()(void){
 						//送信可能なら、送信
 						devmng::courier::uart::fput(vmc1_send(pVMC));
@@ -139,9 +52,114 @@ namespace hmr{
 					}
 				};
 				struct rx_interrput{
-					//データを受信し、Comに処理させる
-					vmc1_recv(pVMC, devmng::courier::uart::fget());
+				private:
+					this_type& Ref;
+				public:
+					void operator()(void){
+						//データを受信し、Comに処理させる
+						vmc1_recv(pVMC, devmng::courier::uart::fget());
+					}
 				};
+			private:
+				struct cDualUart: public io_device_{
+					typedef cUartSwitcher<io_device_> this_type;
+				public:
+				private:
+					tx_interrupt TxInterrupt;
+					rx_interrupt RxInterrupt;
+				private:
+					//レジスタ系
+					xc32::interrupt_uart<RF0_uart_register> RFUart;
+					xc32::interrupt_uart<RF1_uart_register> PhoneUart;
+					RF0_power PinPowerRFUart;
+					RF1_power PinPowerPhoneUart;
+					//現在のモード
+					io::module_mode Mode;
+				public:
+					cDualUart(const tx_interrupt& TxInterrupt_, const rx_interrupt& RxInterrupt_)
+						: TxInterrupt(TxInterrupt_)
+						, RxInterrupt(RxInterrupt_)
+						, Mode(module_null){
+						PinPowerRFUart.lock();
+						PinPowerPhoneUart.lock();
+					}
+					~cDualUart(){
+						setMode(module_null);
+						PinPowerRFUart.unlock();
+						PinPowerPhoneUart.unlock();
+					}
+					void setMode(io::module_mode ModuleMode_){
+						//同じモードなら、無視
+						if(ModuleMode_ == Mode)return;
+
+						Mode = ModuleMode_;
+						switch(Mode){
+						case io::module_mobile:
+							RFUart.unlock();
+							PhoneUart.lock(38400, xc32::uart::flowcontrol::rts_cts_control, SendInterrupt, RecvInterrupt);
+							PinPowerRFUartlock(false);
+							PinPowerPhoneUartlock(true);
+						case io::module_rf:
+							RFUart.lock(9600, xc32::uart::flowcontrol::rts_cts_control, SendInterrupt, RecvInterrupt);
+							PhoneUart.unlock();
+							PinPowerRFUartlock(true);
+							PinPowerPhoneUartlock(false);
+						default:
+							RFUart.unlock();
+							PhoneUart.unlock();
+							PinPowerRFUartlock(false);
+							PinPowerPhoneUartlock(false);
+						}
+					}
+					io::module_mode getMode()const{ return Mode; }
+				};
+			private:
+				//Uart
+				cDualUart Uart;
+				//VMC1
+				VMC1* pVMC;
+				//受け取り用データ
+				idata IData;
+				bool IPacketMode = 0;
+				odata OData;
+				bool OPacketMode = 0;
+				//メッセージホルダー
+				cMessage Message;
+			public:
+				cIO(){
+					//通信関連機能の初期化
+					pVMC = com::createVMC1();
+					vmc1_initialize(pVMC, (const unsigned char*)("hmr"), (const unsigned char*)("ctr"));
+					com::initialize();
+
+					devmng::courier::uart::fput_set_interrupt();//streamVMC_set_fput_interrupt_flag();//set_interrupt_fputflag(Stream_VMC);//割り込み予約
+					devmng::interrupt_enable_streamVMC_fget_interrupt();// enable_interrupt_fget(Stream_VMC);
+					devmng::interrupt_enable_streamVMC_fput_interrupt();// enable_interrupt_fput(Stream_VMC);
+				}
+				~cIO{
+					//割り込み禁止
+					devmng::courier::uart::fget_disable_interrupt();
+					devmng::courier::uart::fput_disable_interrupt();
+
+					//通信関連の終端化処理
+					vmc1_finalize(pVMC);
+					com::finalize();
+					com::releaseVMC1(pVMC);	
+				}
+			public:
+				//message_host_interface
+				virtual void regist(message::element& rElement_){
+					Message.regist(rElement_);
+				}
+			public:
+				//module_selector_interface
+				virtual bool setModuleMode(io::module_mode Mode_){
+					Uart.setMode(Mode_);
+					return false;
+				}
+				virtual module_mode getModuleMode()const{
+					return Uart.getMode();
+				}
 			public:
 				void operator()(void){
 					//受信可能なデータがある場合
@@ -207,145 +225,6 @@ namespace hmr{
 					if(!devmng::courier::uart::fput_is_interrupt_enable()){
 						if(vmc1_can_send(pVMC))devmng::interrupt_enable_streamVMC_fput_interrupt();//uart1_enable_fput_interrupt();
 					}
-				}
-			public://old functions
-				   //=== rf module uart select ===
-				   //現在アクティブなID
-				rf_module_mode rf_module_uart_ModuleID = rf_module_mode::null;
-				//ModuleIDのモジュールで初期化する
-				void rf_module_uart_initialize(rf_module_mode ModuleID_, uint16 BaudRate_){
-					if(ModuleID_ == rf_module_mode::mobile_phone){
-						rf_module_uart_ModuleID = rf_module_mode::mobile_phone;
-						Device.MobilePhone_uart.initialize(Device.MobilePhone_uart_register);
-						Device.MobilePhone_uart.config(Device.MobilePhone_uart_Baudrate, Device.MobilePhone_uart_flowctrl, courier::uart::CourierTxInterruptFunc, courier::uart::CourierRxInterruptFunc);
-						Device.MobilePhone_uart.lock();
-						//Device.RF_uart.release();
-					} else if(ModuleID_ == rf_module_mode::rf_module){
-						rf_module_uart_ModuleID = rf_module_mode::rf_module;
-						Device.RF_uart.initialize(Device.RF_uart_register);
-						Device.RF_uart.config(Device.RF_uart_Baudrate, Device.RF_uart_flowctrl, courier::uart::CourierTxInterruptFunc, courier::uart::CourierRxInterruptFunc);
-						Device.RF_uart.lock();
-						//Device.MobilePhone_uart.release();
-					}
-				}
-				//ModuleIDに電源を変更する
-				void rf_module_uart_power_switch(bool onoff, rf_module_mode ModuleID_){
-					if(onoff && ModuleID_ == rf_module_mode::mobile_phone){
-						Device.PinMobilePhonePower(1);
-						Device.PinRFPower(0);
-					} else if(onoff && ModuleID_ == rf_module_mode::rf_module){
-						Device.PinMobilePhonePower(0);
-						Device.PinRFPower(1);
-					} else{
-						Device.PinMobilePhonePower(0);
-						Device.PinRFPower(0);
-					}
-				}
-				//現在のModuleIDを取得する
-				rf_module_mode rf_module_uart_getModuleID(void){ return rf_module_uart_ModuleID; }
-				// main stream 割り込みの許可禁止をいじくってよいかを返す関数
-				bool interrupt_can_enable_streamVMC_fget_interrupt(){
-					return !(sleep_isSleep() && !sleep_isRem());
-				}
-				bool interrupt_can_disable_streamVMC_fget_interrupt(){
-					return true;//!( sleep_isSleep() && !sleep_isRem() );
-				}
-				bool interrupt_can_enable_streamVMC_fput_interrupt(){
-					return !(sleep_isSleep() && !sleep_isRem());
-				}
-				bool interrupt_can_disable_streamVMC_fput_interrupt(){
-					return true;//!( sleep_isSleep() && !sleep_isRem() );
-				}
-
-				// main stream の割り込み許可禁止関数
-				bool interrupt_enable_streamVMC_fget_interrupt(){
-					if(interrupt_can_enable_streamVMC_fget_interrupt()){
-						if(rf_module_uart_getModuleID() == rf_module_mode::mobile_phone)Device.MobilePhone_uart.recv_enable();
-						else if(rf_module_uart_getModuleID() == rf_module_mode::rf_module)Device.RF_uart.recv_enable();
-						return true;
-					} else{
-						return false;
-					}
-				}
-				bool interrupt_disable_streamVMC_fget_interrupt(){
-					if(interrupt_can_disable_streamVMC_fget_interrupt()){
-						if(rf_module_uart_getModuleID() == rf_module_mode::mobile_phone)Device.MobilePhone_uart.recv_disable();
-						else if(rf_module_uart_getModuleID() == rf_module_mode::rf_module)Device.RF_uart.recv_disable();
-						return true;
-					} else{
-						return false;
-					}
-				}
-				bool interrupt_enable_streamVMC_fput_interrupt(){
-					if(interrupt_can_enable_streamVMC_fput_interrupt()){
-						if(rf_module_uart_getModuleID() == rf_module_mode::mobile_phone)Device.MobilePhone_uart.send_enable();
-						else if(rf_module_uart_getModuleID() == rf_module_mode::rf_module)Device.RF_uart.send_enable();
-						return true;
-					} else{
-						return false;
-					}
-				}
-				bool interrupt_disable_streamVMC_fput_interrupt(){
-					if(interrupt_can_disable_streamVMC_fput_interrupt()){
-						if(rf_module_uart_getModuleID() == rf_module_mode::mobile_phone)Device.MobilePhone_uart.send_disable();
-						else if(rf_module_uart_getModuleID() == rf_module_mode::rf_module)Device.RF_uart.send_disable();
-						return true;
-					} else{
-						return false;
-					}
-				}
-
-				uint8 fget(){
-					if(rf_module_uart_getModuleID() == rf_module_mode::mobile_phone)return Device.MobilePhone_uart.recv_data();
-					else if(rf_module_uart_getModuleID() == rf_module_mode::rf_module)return Device.RF_uart.recv_data();
-					else return 0;
-				}
-				void fget_set_interrupt(){
-					if(rf_module_uart_getModuleID() == rf_module_mode::mobile_phone)Device.MobilePhone_uart.recv_set_flag();
-					else if(rf_module_uart_getModuleID() == rf_module_mode::rf_module)Device.RF_uart.recv_set_flag();
-				}
-				void fget_clear_interrupt(){
-					if(rf_module_uart_getModuleID() == rf_module_mode::mobile_phone)Device.MobilePhone_uart.recv_clear_flag();
-					else if(rf_module_uart_getModuleID() == rf_module_mode::rf_module)Device.RF_uart.recv_clear_flag();
-				}
-				void fget_enable_interrupt(){
-					if(rf_module_uart_getModuleID() == rf_module_mode::mobile_phone)Device.MobilePhone_uart.recv_enable();
-					else if(rf_module_uart_getModuleID() == rf_module_mode::rf_module)Device.RF_uart.recv_enable();
-				}
-				void fget_disable_interrupt(){
-					if(rf_module_uart_getModuleID() == rf_module_mode::mobile_phone)Device.MobilePhone_uart.recv_disable();
-					else if(rf_module_uart_getModuleID() == rf_module_mode::rf_module)Device.RF_uart.recv_disable();
-				}
-				bool fget_is_interrupt_enable(){
-					if(rf_module_uart_getModuleID() == rf_module_mode::mobile_phone)return Device.MobilePhone_uart.recv_is_enable();
-					else if(rf_module_uart_getModuleID() == rf_module_mode::rf_module)return Device.RF_uart.recv_is_enable();
-					else return false;
-				}
-
-				void fput(uint8 data_){
-					if(rf_module_uart_getModuleID() == rf_module_mode::mobile_phone)Device.MobilePhone_uart.send_data(data_);
-					else if(rf_module_uart_getModuleID() == rf_module_mode::rf_module)Device.RF_uart.send_data(data_);
-				}
-				if(rf_module_uart_getModuleID() == rf_module_mode::mobile_phone)Device.MobilePhone_uart.send_set_flag();
-				void fput_set_interrupt(){
-				else if(rf_module_uart_getModuleID() == rf_module_mode::rf_module)Device.RF_uart.send_set_flag();
-				}
-				void fput_clear_interrupt(){
-					if(rf_module_uart_getModuleID() == rf_module_mode::mobile_phone)Device.MobilePhone_uart.send_clear_flag();
-					else if(rf_module_uart_getModuleID() == rf_module_mode::rf_module)Device.RF_uart.send_clear_flag();
-				}
-				void fput_enable_interrupt(){
-					if(rf_module_uart_getModuleID() == rf_module_mode::mobile_phone)Device.MobilePhone_uart.send_enable();
-					else if(rf_module_uart_getModuleID() == rf_module_mode::rf_module)Device.RF_uart.send_enable();
-				}
-				void fput_disable_interrupt(){
-					if(rf_module_uart_getModuleID() == rf_module_mode::mobile_phone)Device.MobilePhone_uart.send_disable();
-					else if(rf_module_uart_getModuleID() == rf_module_mode::rf_module)Device.RF_uart.send_disable();
-				}
-				bool fput_is_interrupt_enable(){
-					if(rf_module_uart_getModuleID() == rf_module_mode::mobile_phone)return Device.MobilePhone_uart.send_is_enable();
-					else if(rf_module_uart_getModuleID() == rf_module_mode::rf_module)return Device.RF_uart.send_is_enable();
-					else return false;
 				}
 			public:
 				//現在の通信先が全二重通信対応かを返す
